113cd8c289f335e2b19a27ff4810189c62ad6589
Omowunmijuin <balikiskamil@gmail.com>
2022-12-11 16:48:25 +0000

    I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Print all the letters except q and e
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	if (c != q && c != e)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
