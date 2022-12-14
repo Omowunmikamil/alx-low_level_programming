c5827064c92606457c5428f401eec8942e47e24a
Omowunmijuin <balikiskamil@gmail.com>
2022-12-14 12:14:26 -0800

    The World looks like a multiplication-table, or a mathematical equation

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,13 +11,13 @@ void print_times_table(int n)
 {
 	int a, b, c;
 
-	if (n >= 0 && n >= 15)
+	if (n >= 0 && n <= 15)
 	{
 		for (a = 0; a <= n; a++)
 		{
 			for (b = 0; b <= n; b++)
 			{
-				c = a * b;
+				c = b * a;
 				if (b == 0)
 				{
 					_putchar(c + '0');
