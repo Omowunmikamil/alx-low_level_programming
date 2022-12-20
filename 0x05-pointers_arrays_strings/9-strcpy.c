817afdd2bf1e754de029bbb37a2a26302214ce7c
Omowunmijuin <balikiskamil@gmail.com>
2022-12-20 13:30:21 -0800

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copies the string pointed to by src, including
+ * the terminating null to the buffer
+ * @dest: pointer to to the string of array
+ * @src: num of element to extract
+ * Return: 0
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int x = 0;
+
+	while (*(src + x) != '\0')
+	{
+		*(dest + x) = *(src + x);
+		x++
+	}
+	*(dest + x) = '\0';
+	return (dest);
+}
+
