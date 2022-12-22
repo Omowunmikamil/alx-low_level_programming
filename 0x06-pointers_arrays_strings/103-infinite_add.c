37c7d4dc4e8e23a4f4fec19761e9a17bb64aa21c
Omowunmijuin <balikiskamil@gmail.com>
2022-12-22 06:14:51 -0800

    It is the addition of strangeness to beauty that constitutes the romantic character in art

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -6,46 +6,44 @@
  * @n2: num 2
  * @r: result
  * @size_r: size result
- * Return: r
+ * Return: r or 0
 */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int x = 0, y = 0, a, b, c, n, aux, dec = 0;
+	int a, b, c, d, e, f;
 
-	while (n1[x] != '\0')
+	for (a = 0; n1[a]; a++)
+		;
+	for (b = 0; n2[b]; b++)
+		;
+	if (a > size_r || b > size_r)
 	{
-		x++
+		return (0);
 	}
-	while (n2[y] != '\0')
+	e = 0;
+	for (a -= 1, b -= 1, c = 0; c < size_r - 1; a--, b--, c++)
 	{
-		y++;
-	}
-	for (n = 0; n < y || n < x; n++)
-	{
-		a = (x - n) > 0 ? (n1[x - n - 1] - '0') : 0;
-		b = (y - n) > 0 ? (n2[y - n - 1] - '0') : 0;
-		c = a + b + dec;
-		r[n] = (c % 10) + '0';
-		dec = c > 9 ? 1 : 0;
-	}
-	if (dec == 1)
-	{
-		r[n] = '1';
-		r[n + 1] = '\0';
-	} else
-	{
-		r[n] = '\0';
-		n--;
-	}
-	for (x = 0; x < n + 1; x++)
-	{
-		for (y = 0; y < (n - x); y++)
+		f = e;
+		if (a >= 0)
+			f += n1[a] - '0';
+		if (b >= 0)
+			f += n2[b] - '0';
+		if (a < 0 && b < 0 && f == 0)
 		{
-			aux = r[y + 1];
-			r[y + 1] = r[y];
-			r[y] = aux;
+			break;
 		}
+		e = f / 10;
+		r[c] = f % 10 + '0';
 	}
-return (n < size_r - 1 ? r : 0);
+	r[c] = '\0';
+	if (a >= 0 || b >= 0 || e)
+		return (0);
+	for (c -= 1, d = 0; d < c; c--, d++)
+	{
+		e = r[c];
+		r[c] = r[d];
+		r[d] = e;
+	}
+	return (r);
 }
