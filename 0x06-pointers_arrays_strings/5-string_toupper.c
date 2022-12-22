846d079c7b693e08c92f0ff71ebfd8d26a0440da
Omowunmijuin <balikiskamil@gmail.com>
2022-12-21 17:50:45 -0800

    Always look up

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * *string_toupper - changes all lowercase letters of a string to uppercase
+ * string_toupper - changes all lowercase letters of a string to uppercase
  * @s: sring to be changed
  * Return: s
 */
@@ -14,7 +14,7 @@ char *string_toupper(char *s)
 	{
 		if (*(s + j) >= 'a' && *(s + j) <= 'z')
 		{
-			*(s + j) -= 'g' - 'A';
+			*(s + j) -= 'a' - 'A';
 			j++
 		}
 	}
