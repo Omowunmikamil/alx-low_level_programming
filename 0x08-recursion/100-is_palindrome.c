ec5873fd37177e7be401be87cf7bfa6a598c9804
Omowunmijuin <balikiskamil@gmail.com>
2023-01-04 18:25:06 -0800

    When we're asleep, we can do almost anything

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * is_palindrome - takes pointer to int
+ * @s: accespts input saved in s
+ * Return: 0
+*/
+
+int is_palindrome(char *s)
+{
+	if (*s == 0)
+		return (1);
+	return (check_pal(s, 0, _strlen_recursion(s)));
+}
+
+/**
+ * _strlen_recursion - returns the length
+ * @s: string to calculate the length
+ * Return: length of the string
+*/
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+ * check_pal - checks character recursively
+ * @s: string
+ * @i: iterator
+ * @len: length
+ * Return: 1 if palindrome or 0
+*/
+
+int check_pal(char *s, int i, int len)
+{
+	if (*(s + i) != *(s + len - 1))
+		return (0);
+	if (i >= len)
+		return (1);
+
+	return (check_pal(s, i + 1, len - 1));
+}
