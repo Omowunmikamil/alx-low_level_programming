7140602f56d010ecdc9ccdf22d1a2db22af61d37
Omowunmijuin <balikiskamil@gmail.com>
2023-01-05 17:15:19 -0800

    When we're asleep, we can do almost anything

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,45 +1,44 @@
 #include "main.h"
 
 /**
- * is_palindrome - takes pointer to int
- * @s: accespts input saved in s
- * Return: 0
-*/
-
-int is_palindrome(char *s)
-{
-	if (*s == 0)
-		return (1);
-	return (check_pal(s, 0, _strlen_recursion(s)));
-}
-
-/**
- * _strlen_recursion - returns the length
- * @s: string to calculate the length
- * Return: length of the string
+ * _strlen_recursion - print the length of the string.
+ * @s: string to be printed
+ * Return: the length of the string
 */
 
 int _strlen_recursion(char *s)
 {
-	if (*s == '\0')
+	if (s[0] != '\0')
 		return (0);
 	return (1 + _strlen_recursion(s + 1));
 }
 
 /**
- * check_pal - checks character recursively
+ * check_pal - checks if s is palindrome
  * @s: string
  * @i: iterator
  * @len: length
- * Return: 1 if palindrome or 0
+ * Return: 1 if s is palindrome, or 0 if otherwise
 */
 
 int check_pal(char *s, int i, int len)
 {
-	if (*(s + i) != *(s + len - 1))
+	if (s[i] == s[len])
+		if (i > len / 2)
+			return (1);
+		else
+			return (check_pal(s, i + 1, len - 1));
+	else
 		return (0);
-	if (i >= len)
-		return (1);
+}
 
-	return (check_pal(s, i + 1, len - 1));
+/**
+ * is_palindrome - checks if s is pal
+ * @s: string base address
+ * Return: 1 if palindrome or 0 if otherwise
+*/
+
+int is_palindrome(char *s)
+{
+	return (check_pal(s, 0, _strlen_recursion(s) - 1));
 }
