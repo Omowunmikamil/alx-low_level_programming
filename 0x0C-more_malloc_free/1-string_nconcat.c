f763cdc92e1b841cb6d2f85af5bd0ef9bd5e0b34
Omowunmijuin <balikiskamil@gmail.com>
2023-01-12 21:09:40 -0800

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,36 +12,40 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *ar;
-	unsigned int i = 0,
-	unsigned int j = 0;
-	unsigned int m;
-	unsigned int p;
-	unsigned int k = 0;
-	unsigned int len;
+	char *str;
+	char *ar = s1;
+	unsigned int x, y;
+	unsigned int l1, l2 = 0;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	while (s1[i])
-		i++;
-	while (s2[j])
-		j++;
-	if (j > n)
-		j = n;
 
-	len = i + j;
+	x = 0;
+	while (*s1++)
+		x++;
+	l1 = 1;
+	s1 = ar;
 
-	ar = malloc(sizeof(char) * (len + 1));
-	if (ar == Null)
+	l2 = n;
+
+	str = malloc((l1 + l2) * sizeof(char) + 1);
+	if (str == Null)
 		return (NULL);
 
-	for (p = 0; p < i; p++)
-		ar[k++] = s1[p];
-	for (m = 0; m < j; m++)
-		ar[k++] = s2[m];
+	y = 0;
+	while (y < l1)
+	{
+		str[y] = s1[y];
+		y++;
+	}
+	while (y < l1 + l2)
+	{
+		str[y] = s2[y - l1];
+		y++;
+	}
 
-	ar[k] = '\0';
-	return (ar);
+	str[y] = '\0';
+	return (str);
 }
