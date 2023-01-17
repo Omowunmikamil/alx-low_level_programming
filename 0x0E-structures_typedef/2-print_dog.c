f30e4acc35be433e5ce505be65821f0d176e078a
Omowunmijuin <balikiskamil@gmail.com>
2023-01-16 17:57:20 -0800

    A dog will teach you unconditional love. If you can have that in your life, things won't be too bad

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,7 +13,7 @@ void print_dog(struct dog *d)
 	if (d)
 	{
 		if (d->name)
-			printf("Name: %\n", d->name);
+			printf("Name: %s\n", d->name);
 		else
 			printf("Name: (nil)\n");
 		printf("Age: %.6f\n", d->age);
