3f7d3e5e09d29cc85663e38067ed051af7c1e993
Omowunmijuin <balikiskamil@gmail.com>
2023-01-19 20:21:55 -0800

    variable function

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,20 @@
 #ifndef _VARIADIC_FUNCTIONS_H
 #define _VARIADIC_FUNCTIONS_H
 
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * struck check - parameters checker
+ * @c: type
+ * @f: recieving function
+*/
+typeof struct check
+{
+	char *chk;
+	void (*f)(va_list list);
+} check_t;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
