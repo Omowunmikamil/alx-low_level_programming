65f5326ae58169591e3aef3e4e69392b043af58f
Omowunmijuin <balikiskamil@gmail.com>
2023-01-19 18:24:14 -0800

    To be is a to be the value of a variable

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -11,7 +11,7 @@
 void print_all(const char * const format, ...)
 {
 	va_list ap;
-	unsigned int i = 0, j, n = 0;
+	unsigned int i = 0, j, c = 0;
 	char *str;
 	const char t_arg[] = "cifs";
 
@@ -21,7 +21,7 @@ void print_all(const char * const format, ...)
 		j = 0;
 		while (t_arg[j])
 		{
-			if (format[i] == t_arg[j] && n)
+			if (format[i] == t_arg[j] && c)
 			{
 				printf(", ");
 				break;
@@ -30,16 +30,16 @@ void print_all(const char * const format, ...)
 		switch (format[i])
 		{
 			case 'c':
-				printf("%c", va_arg(ap, int)), n = 1;
+				printf("%c", va_arg(ap, int)), c = 1;
 				break;
 			case 'i':
-				printf("%d", va_arg(ap, int)), n = 1;
+				printf("%d", va_arg(ap, int)), c = 1;
 				break;
 			case 'f':
-				printf("%f", va_arg(ap, double)), n = 1;
+				printf("%f", va_arg(ap, double)), c = 1;
 				break;
 			case 's':
-				str = va_arg(ap, char *), n = 1;
+				str = va_arg(ap, char *), c = 1;
 				if (!str)
 				{
 					printf("(nil)");
