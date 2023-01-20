1d9d775fd656c55fce11b0df1a35bf20d0e15b19
Omowunmijuin <balikiskamil@gmail.com>
2023-01-19 18:41:01 -0800

    To be is a to be the value of a variable

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,5 +1,4 @@
 #include <stdarg.h>
-#include <stdio.h>
 #include "variadic_functions.h"
 
 /**
@@ -10,12 +9,12 @@
 
 void print_all(const char * const format, ...)
 {
-	va_list ap;
+	va_list valist;
 	unsigned int i = 0, j, c = 0;
 	char *str;
 	const char t_arg[] = "cifs";
 
-	va_start(ap, format);
+	va_start(valist, format);
 	while (format && format[i])
 	{
 		j = 0;
@@ -29,25 +28,25 @@ void print_all(const char * const format, ...)
 		}
 		switch (format[i])
 		{
-			case 'c':
-				printf("%c", va_arg(ap, int)), c = 1;
-				break;
-			case 'i':
-				printf("%d", va_arg(ap, int)), c = 1;
-				break;
-			case 'f':
-				printf("%f", va_arg(ap, double)), c = 1;
-				break;
-			case 's':
-				str = va_arg(ap, char *), c = 1;
-				if (!str)
-				{
-					printf("(nil)");
-					break;
-				}
-				printf("%s", str);
-				break;
+		case 'c':
+			printf("%c", va_arg(valist, int)), c = 1;
+			break;
+		case 'i':
+			printf("%d", va_arg(valist, int)), c = 1;
+			break;
+		case 'f':
+			printf("%f", va_arg(valist, double)), c = 1;
+			break;
+		case 's':
+			str = va_arg(valist, char *), c = 1;
+			if (!str)
+			{
+			printf("(nil)");
+			break;
+			}
+			printf("%s", str);
+			break;
 		} i++
 	}
-	printf("\n"), va_end(ap);
+	printf("\n"), va_end(valist);
 }
