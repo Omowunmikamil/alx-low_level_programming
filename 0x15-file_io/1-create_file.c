5dcbafc980c4646d0b01230c7ae4dda5ffaa216b
Omowunmijuin <balikiskamil@gmail.com>
2023-02-06 16:22:50 -0800

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONGLY | O_TRUNG, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNG, 0600);
 
 	if (fd == -1)
 		return (-1);
