7e186e05e9eb8eb64746cd454fdabe846a4cc74c
Omowunmijuin <balikiskamil@gmail.com>
2023-04-07 19:20:46 -0700

    >>> ht = {}

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -3,34 +3,34 @@
 /**
  * hash_table_create - creates a hash table.
  *
- * @size: Hash table size
+ * @size: size of array
  * Return: Pointer to the newly created hash table.
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hash_table = NULL;
-	hash_node_t **array = NULL;
+	hash_table_t *hash_table;
 	unsigned long int index;
 
 
-	/* allocate table */
+	/* allocate memory for hash table */
 	hash_table = malloc(sizeof(hash_table_t));
 	if (hash_table == NULL)
 		return (NULL);
 
-	hash_table->size = size;
-
-	/* allocate array */
-	array = malloc(size * sizeof(hash_node_t *));
-	if (array == NULL)
+	/* allocate memory for array */
+	hash_table->array = malloc(size * sizeof(hash_node_t *));
+	if (hash_table->array == NULL)
+	{
+	free(hash_table);
 		return (NULL);
+	}
 
 	/* initialize array */
 	for (index = 0; index < size; index++)
-		array[index] = NULL;
+		hash_table->array[index] = NULL;
 
-
-	hash_table->array = array;
+	/* set the size of the hash table */
+	hash_table->size = size;
 
 	return (hash_table);
 }
